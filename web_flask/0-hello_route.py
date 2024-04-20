ac1effe3974fcf879f240fe4b6557dbbbbd72ce8
Ramyrashad127 <ramy41599@gmail.com>
2024-04-21 01:39:46 +0200

    new

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+from flask import Flask
+fl = Flask(__name__)
+@fl.route('/', strict_slashes=False)
+def say_hellow():
+    return "Hello HBNB!"
+if __name__ == '__main__':
+    fl.run(host='0.0.0.0', port=5000)
