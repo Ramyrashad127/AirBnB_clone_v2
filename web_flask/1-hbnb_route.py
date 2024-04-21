0e77f20aee84c425a0ca7b464fcee4239327ad03
Ramyrashad127 <ramy41599@gmail.com>
2024-04-21 02:04:12 +0200

    new

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#@/usr/bin/python3
+""" import modules"""
+
+
+
+from flask import Flask
+fl = Flask(__name__)
+
+
+@fl.route('/', strict_slashes=False)
+def say_hellow():
+    """ new function"""
+    return "Hello HBNB!"
+
+
+@fl.route('//hbnb',strict_slashes=False)
+def hbnb():
+    """new tab"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    fl.run(host='0.0.0.0', port=5000)
