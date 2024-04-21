3561337ee75d4ad0892707beac9da3027f8fe3c3
Ramyrashad127 <ramy41599@gmail.com>
2024-04-21 15:01:00 +0200

    new

--- models/state.py
+++ models/state.py
@@ -12,9 +12,20 @@ import models
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    if models.storage_t == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",  backref="state", cascade="delete")
+    else:
+        name = ""
+    
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+
+
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
