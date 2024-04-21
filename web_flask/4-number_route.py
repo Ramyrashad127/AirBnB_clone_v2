f5708b6f7f441f3dcd11d412feaa9229747649e9
Ramyrashad127 <ramy41599@gmail.com>
2024-04-21 02:49:43 +0200

    new

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" import modules"""
+
+
+from flask import Flask
+from urllib.parse import unquote
+fl = Flask(__name__)
+
+
+@fl.route('/', strict_slashes=False)
+def say_hellow():
+    """ new function"""
+    return "Hello HBNB!"
+
+
+@fl.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """new tab"""
+    return "HBNB"
+
+
+@fl.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """new function"""
+    new = unquote(text.replace('_', ' '))
+    return "C {}".format(new)
+
+
+@fl.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@fl.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """new function"""
+    new = text.replace('_', ' ')
+    return "Python {}".format(new)
+
+
+@fl.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """print a number"""
+    if(isinstance(n,int)):
+        return "{} is a number".format(n)
+
+
+
+if __name__ == '__main__':
+    fl.run(host='0.0.0.0', port=5000)
